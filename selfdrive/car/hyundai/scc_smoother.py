5b302ab6d3c7dd2e21f550f90bffda00f97d2f6e
gallcar <76591391+gallcar@users.noreply.github.com>
2020-12-28 22:00:46 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -44,6 +44,15 @@ class SccSmoother:
       AliveIndex = 0
     return count
 
+  @staticmethod
+  def get_wait_count():
+    global WaitIndex
+    count = WAIT_COUNT[WaitIndex]
+    WaitIndex += 1
+    if WaitIndex >= len(WAIT_COUNT):
+      WaitIndex = 0
+    return count
+
   def __init__(self, accel_gain, decel_gain, curvature_gain):
 
     self.accel_gain = accel_gain
@@ -99,7 +108,7 @@ class SccSmoother:
     return packer.make_can_msg("CLU11", bus, values)
 
   def is_active(self, frame):
-    return frame - self.started_frame <= ALIVE_COUNT + max(WAIT_COUNT)
+    return frame - self.started_frame <= max(ALIVE_COUNT) + max(WAIT_COUNT)
 
   def dispatch_buttons(self, CC, CS):
     changed = False
@@ -188,9 +197,9 @@ class SccSmoother:
       #  .format(float(apply_accel*CV.MS_TO_KPH), int(CS.acc_mode), int(enabled), int(CS.cruiseState_enabled), int(CS.standstill), float(CS.cruiseState_speed),
       #          int(CS.cruise_buttons), int(CS.brake_pressed), int(CS.gas_pressed))
 
-      CC.sccSmoother.logMessage = max_speed_log
+      CC.sccSmoother.logMessage = ""
       self.reset()
-      self.wait_timer = ALIVE_COUNT + max(WAIT_COUNT)
+      self.wait_timer = max(ALIVE_COUNT) + max(WAIT_COUNT)
       return
 
     current_set_speed = CS.cruiseState_speed * CV.MS_TO_KPH
@@ -218,7 +227,7 @@ class SccSmoother:
 
       if self.alive_timer == 0:
         self.btn = self.get_button(clu11_speed, current_set_speed)
-        self.alive_count = ALIVE_COUNT
+        self.alive_count = SccSmoother.get_alive_count()
 
       if self.btn != Buttons.NONE:
         can_sends.append(SccSmoother.create_clu11(packer, self.alive_timer, CS.scc_bus, CS.clu11, self.btn))
@@ -293,8 +302,7 @@ class SccSmoother:
         override_acc = acc
         accel = (op_accel + acc) / 2.
       else:
-        accel = op_accel * interp(clu11_speed, [0., 30., 38., 50., 51., 60., 100.],
-                                  [2.3, 3.4, 3.2, 1.7, 1.65, 1.4, 1.0])
+        accel = op_accel * interp(clu11_speed, [0., 30., 38., 50., 51., 60., 100.], [2.3, 3.4, 3.2, 1.7, 1.65, 1.4, 1.0])
 
     if accel > 0.:
       accel *= self.accel_gain * interp(clu11_speed, [35., 60., 100.], [1.5, 1.25, 1.2])
@@ -381,5 +389,4 @@ class SccSmoother:
       v_cruise_kph = clip(v_cruise_kph, MIN_SET_SPEED, MAX_SET_SPEED)
 
     return v_cruise_kph
-
-
+  
